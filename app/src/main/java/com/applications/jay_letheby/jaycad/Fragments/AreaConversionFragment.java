16e0ea61d68d4f57111c9da1bf39d9c558b1e90b
Jay Letheby <jay.letheby@gmail.com>
2017-02-01 21:42:33 +1000

    Area Conversion screen

--- app/src/main/java/com/applications/jay_letheby/jaycad/Fragments/AreaConversionFragment.java
+++ app/src/main/java/com/applications/jay_letheby/jaycad/Fragments/AreaConversionFragment.java
@@ -1,16 +1,33 @@
 package com.applications.jay_letheby.jaycad.Fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.renderscript.Double2;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.applications.jay_letheby.jaycad.Activities.MainActivity;
+import com.applications.jay_letheby.jaycad.HelperClasses.Converter;
 import com.applications.jay_letheby.jaycad.R;
+import com.applications.jay_letheby.jaycad.HelperClasses.DataInputChecker;
+import com.applications.jay_letheby.jaycad.HelperClasses.InputFilterMinMax;
+import com.applications.jay_letheby.jaycad.HelperClasses.DecimalUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,7 +37,7 @@ import com.applications.jay_letheby.jaycad.R;
  * Use the {@link AreaConversionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AreaConversionFragment extends Fragment implements View.OnClickListener{
+public class AreaConversionFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +47,36 @@ public class AreaConversionFragment extends Fragment implements View.OnClickList
     private String mParam1;
     private String mParam2;
 
+    private Button clearConvertToBtn;
+    private Button clearConvertFromBtn;
+    private Button clearStackBtn;
+    private Button convertBtn;
     private Button mainMenuBtn;
+
+    private EditText convertFromTxt;
+    private EditText roodTxt;
+    private EditText perchTxt;
+
+    private TextView convertUnitTxtView;
+    private TextView roodTxtView;
+    private TextView perchTxtView;
+    private TextView converToTxtView;
+    private TextView areaStackTxtView;
+
+    private TextView runningTotalTxtView;
+    private TextView converToLabelTxtView;
+    private TextView stackLabelTxtView;
+    private TextView runningTotalLabelTxtView;
+
+    private Spinner areaConversionSpinner;
+
+    // Keeps running total of measurements in the stack
+    private double runningTotal = 0;
+
+    // Helper class objects
+    private DataInputChecker dataInputChecker;
+    private Converter converter;
+
 
     private AreaConversionFragmentInteractionListener mListener;
 
@@ -71,7 +117,42 @@ public class AreaConversionFragment extends Fragment implements View.OnClickList
 
         View view = inflater.inflate(R.layout.fragment_area_conversion, container, false);
 
+        // Initailise buttons
         mainMenuBtn = (Button)view.findViewById(R.id.mainMenuBtn);
+        convertBtn = (Button)view.findViewById(R.id.convertBtn);
+        clearConvertFromBtn = (Button)view.findViewById(R.id.clearConvertFromBtn);
+        clearConvertToBtn = (Button)view.findViewById(R.id.clearResultBtn);
+        clearStackBtn = (Button)view.findViewById(R.id.clearStackBtn);
+
+        // Initialise EditTextField
+        convertFromTxt = (EditText)view.findViewById(R.id.convertFrom);
+        roodTxt = (EditText)view.findViewById(R.id.roodTxt);
+        perchTxt = (EditText)view.findViewById(R.id.perchTxt);
+
+        // Initialise TextViews
+        convertUnitTxtView = (TextView)view.findViewById(R.id.convertUnitTxtView);
+        roodTxtView = (TextView)view.findViewById(R.id.roodTxtView);
+        perchTxtView = (TextView)view.findViewById(R.id.perchTxtView);
+        converToTxtView = (TextView)view.findViewById(R.id.resultTxtView);
+        areaStackTxtView = (TextView)view.findViewById(R.id.resultStackTxtView);
+        runningTotalTxtView = (TextView)view.findViewById(R.id.runningTotalTxtView);
+        converToLabelTxtView = (TextView)view.findViewById(R.id.converToLabel);
+        stackLabelTxtView = (TextView)view.findViewById(R.id.stackLabelTxtView);
+        runningTotalLabelTxtView = (TextView)view.findViewById(R.id.runTotalLabelTxtView);
+
+        // Initialise spinner
+        areaConversionSpinner = (Spinner)view.findViewById(R.id.units2Spinner);
+        areaConversionSpinner.setOnItemSelectedListener(this);
+
+        // Create adapter for spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.area_units_array, android.R.layout.simple_spinner_dropdown_item);
+
+        // Drop down layout style
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        areaConversionSpinner.setAdapter(adapter);
 
         mainMenuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,12 +163,162 @@ public class AreaConversionFragment extends Fragment implements View.OnClickList
             }
         });
 
+        // Set Listeners for buttons
+        convertBtn.setOnClickListener(this);
+        clearConvertFromBtn.setOnClickListener(this);
+        clearConvertToBtn.setOnClickListener(this);
+        clearStackBtn.setOnClickListener(this);
+
+        // initalise helper class objects
+        dataInputChecker = new DataInputChecker();
+        converter = new Converter();
+
+        // Set default position of spinner as "Acres to Hectares"
+        areaConversionSpinner.setSelection(0);
+
+        // Limit the input to 2 digits for inch and Fraction fields
+        roodTxt.setFilters(new InputFilter[] {new InputFilter.LengthFilter(1)});
+        perchTxt.setFilters(new InputFilter[] {new InputFilter.LengthFilter(2)});
+
+        // Limit the number range to 0-11 for roodTxt fields
+        roodTxt.setFilters(new InputFilter[]{new InputFilterMinMax("0", "3")});
+
+        // Limit the number range to 0-15 for perch fields
+        perchTxt.setFilters(new InputFilter[]{new InputFilterMinMax("0", "39")});
+
+        // Set Length Stack to be scrollable
+        areaStackTxtView.setMovementMethod(new ScrollingMovementMethod());
+
         // Inflate the layout for this fragment
         return view;
     }
 
     @Override
     public void onClick(View view) {
+        Button chosenBtn = (Button)view;
+
+        if (chosenBtn == convertBtn){
+            // convert units
+            //convertMeasurement();
+        } else if (chosenBtn == clearConvertFromBtn) {
+            convertFromTxt.setText("");
+            roodTxt.setText("");
+            perchTxt.setText("");
+        }
+        else if (chosenBtn == clearConvertToBtn)
+            converToTxtView.setText("");
+        else if (chosenBtn == clearStackBtn) {
+            areaStackTxtView.setText("");
+            runningTotalTxtView.setText("");
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        clearAll();
+
+        switch(position) {
+            case 0:
+                // Feet to Meters
+                convertUnitTxtView.setText("FEET");
+                convertFromTxt.setInputType(InputType.TYPE_CLASS_NUMBER);
+                showRoodPerchFields();
+                clearInputFields();
+                changeResultsLabels("METRES");
+                break;
+            case 1:
+                // Metres to feet
+                convertUnitTxtView.setText("METRES");
+                // Allow decimal input from metres
+                convertFromTxt.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                // Hide Inch fields until selected from dropdown
+                hideRoodPerchFields();
+                clearInputFields();
+                changeResultsLabels("FEET");
+                break;
+            case 2:
+                // Links to Mtres
+                convertUnitTxtView.setText("LINKS");
+                // Allow decimal input from Links
+                convertFromTxt.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                // Hide Inch fields until selected from dropdown
+                hideRoodPerchFields();
+                clearInputFields();
+                changeResultsLabels("METRES");
+                break;
+            case 3:
+                // Metres to Links
+                convertUnitTxtView.setText("METRES");
+                // Allow decimal input from metres
+                convertFromTxt.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                // Hide Inch fields until selected from dropdown
+                hideRoodPerchFields();
+                clearInputFields();
+                changeResultsLabels("LINKS");
+                break;
+        }
+    }
+
+    public void hideRoodPerchFields (){
+        // Hide Rood and Perches fields and field lables
+        roodTxtView.setVisibility(View.INVISIBLE);
+        perchTxtView.setVisibility(View.INVISIBLE);
+        roodTxt.setVisibility(View.INVISIBLE);
+        perchTxt.setVisibility(View.INVISIBLE);
+
+        roodTxt.setEnabled(false);
+        perchTxt.setEnabled(false);
+
+    }
+
+    public void showRoodPerchFields () {
+        // Show Rood and Perches fields and field lables
+        convertUnitTxtView.setText("FEET");
+        convertFromTxt.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        roodTxtView.setVisibility(View.VISIBLE);
+        perchTxtView.setVisibility(View.VISIBLE);
+        roodTxt.setVisibility(View.VISIBLE);
+        perchTxt.setVisibility(View.VISIBLE);
+
+        roodTxt.setEnabled(true);
+        perchTxt.setEnabled(true);
+
+    }
+
+    public void clearInputFields() {
+        // Clear input fields only
+        convertFromTxt.setText("");
+        roodTxt.setText("");
+        perchTxt.setText("");
+    }
+
+    public void changeResultsLabels (String measurement) {
+        // change labels according to conversion chosen
+        converToLabelTxtView.setText(measurement);
+        stackLabelTxtView.setText("AREA STACK" + " (" + measurement + ")");
+        runningTotalLabelTxtView.setText("RUNNING TOTAL" + " (" + measurement + ")");
+    }
+
+    public void clearAll () {
+        // Clears all EditText Fields and Results, Length Stack
+        // and Running Stack Text Views
+
+        convertFromTxt.setText("");
+        roodTxt.setText("");
+        perchTxt.setText("");
+
+        areaStackTxtView.setText("");
+        runningTotalTxtView.setText("");
+        converToTxtView.setText("");
+
+        runningTotal = 0.0;
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 
